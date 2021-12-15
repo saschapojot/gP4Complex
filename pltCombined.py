f441b2d3d60cf763a347cd6d871bf046fc412858
saschapojot <1992xi.liu@gmail.com>
2021-12-15 16:58:00 +0800

    plots

---
+++ pltCombined.py
@@ -0,0 +1,46 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#load shooting data
+prefix="startL4"
+shootingDf=pd.read_csv(prefix+"shootingR12.csv")
+
+gShooting=shootingDf["g"]
+EShooting=shootingDf["E"]
+##############load adj data frames
+levelFirst=0
+levelLast=5
+# adjDfList=[]
+gAdjAll=[]
+EReAdjAll=[]
+for j in range(levelFirst,levelLast+1):
+    adjDfTmp=pd.read_csv("level"+str(j)+"adjGComplexR12.csv")
+    gAdjAll.extend(list(adjDfTmp["g"]))
+    EReAdjAll.extend(list(adjDfTmp["ERe"]))
+
+
+#############
+fig, ax = plt.subplots(figsize=(20, 20))
+ax.set_ylabel("E")
+# plt.yscale('symlog')
+ax.set_xscale("log")
+ax.set_xlabel("g")
+ax.set_title("Eigenvalues for potential $V(x)=\lambda x^{2}-ix^{5}$")
+
+
+sctShooting=ax.scatter(gShooting,EShooting,color="blue",marker=".",s=40,label="Shooting")
+sctAdjReAll=ax.scatter(gAdjAll,EReAdjAll,color="fuchsia",marker="+",s=50,label="WKB adj real")
+plt.legend()
+plt.savefig("all"+"tmp12.png")
+plt.close()
+
+fig1,ax1=plt.subplots(figsize=(20,20))
+ax1.set_ylabel("E")
+# plt.yscale('symlog')
+ax1.set_xscale("log")
+ax1.set_xlabel("g")
+ax1.set_title("Eigenvalues for potential $V(x)=\lambda x^{2}-ix^{5}$")
+sctAdjReAll1=ax1.scatter(gAdjAll,EReAdjAll,color="fuchsia",marker="+",s=50,label="WKB adj real")
+plt.legend()
+plt.savefig("real"+"tmp12.png")
+plt.close()
---
+++ pltR12.py
@@ -0,0 +1,35 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#load shooting data
+prefix="startL4"
+shootingDf=pd.read_csv(prefix+"shootingR12.csv")
+
+gShooting=shootingDf["g"]
+EShooting=shootingDf["E"]
+################load adj data
+levelNum=7
+adjDf=pd.read_csv("level"+str(levelNum)+"adjGComplexR12.csv")
+gAdj=adjDf["g"]
+EReAdj=adjDf["ERe"]
+EImAdj=adjDf["EIm"]
+#############
+fig, ax = plt.subplots(figsize=(20, 20))
+ax.set_ylabel("E")
+# plt.yscale('symlog')
+ax.set_xscale("log")
+ax.set_xlabel("g")
+ax.set_title("Eigenvalues for potential $V(x)=\lambda x^{2}-ix^{5}$")
+
+
+
+
+
+
+sctShooting=ax.scatter(gShooting,EShooting,color="blue",marker=".",s=40,label="Shooting")
+sctAdjRe=ax.scatter(gAdj,EReAdj,color="fuchsia",marker="+",s=50,label="WKB adj real")
+sctAdjIm=ax.scatter(gAdj,EImAdj,color="green",marker="^",s=50,label="WKB adj imag")
+
+ax.hlines(y=0, xmin=1e-4, xmax=10, color='r')
+plt.legend()
+plt.savefig("n="+str(levelNum)+"tmp12.png")
